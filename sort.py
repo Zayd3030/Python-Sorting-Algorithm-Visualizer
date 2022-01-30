1a791a1359a25937a6ce9fa666503e1f1f6012c7
Zayd Hussain <76216005+Zayd3030@users.noreply.github.com>
2022-01-30 22:14:43 +0000

    Drawing

--- sort.py
+++ sort.py
@@ -1,4 +1,3 @@
-from matplotlib.pyplot import draw
 import pygame
 import random
 pygame.init()
@@ -10,6 +9,7 @@ class DrawInfomation:
     RED = 255, 0, 0
     GREY = 128, 128, 128
     BACKGROUND_COLOUR = WHITE
+    
     SIDE_PAD = 100
     TOP_PAD = 150
 
@@ -22,13 +22,16 @@ class DrawInfomation:
         self.set_list(lst)
     
     def set_list(self, lst):
-        self.lst + lst
+        self.lst = lst
         self.min_val = min(lst)
         self.max_val = max(lst)
 
         self.block_width = round((self.width - self.SIDE_PAD) / len(lst))
         self.block_height = round((self.height - self.TOP_PAD) / (self.max_val - self.min_val))
         self.start_x = self.SIDE_PAD // 2
+
+def draw(draw_info):
+    draw_info.window.fill(draw_info.BACKGROUND_COLOUR)
 
 def generate_starting_list(n, min_val, max_val):
     lst = []
@@ -53,13 +56,13 @@ def main():
     while run:
         clock.tick(60)
 
-        pygame.display.update()
+        
 
         for event in pygame.event.get():
-            if event == pygame.QUIT:
+            if event.type == pygame.QUIT:
                 run = False
 
     pygame.quit()
 
-if __name__== "__main__":
+if __name__ == "__main__":
     main()
